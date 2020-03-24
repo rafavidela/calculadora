1c4b99069f0b66fefcf62285dc24d0b42ce5ef0a
rafavidela <rvidela@gmail.com>
2020-03-24 18:39:35 -0300

    completo con historial

--- app.js
+++ app.js
@@ -2,29 +2,32 @@ const calculadora = require('./calculadora.js');
 const fs=require("fs");
 const logsJSON=fs.readFileSync('./historial.json','utf-8');
 const logs=JSON.parse(logsJSON);
+const [,,operacion,...operandos]=process.argv;
 
 
-
-let operacion = process.argv[2];
-let numero1 = Number(process.argv[3]);
-let numero2 = Numberprocess.argv[4]);
+var operandosNum=operandos.map(function(value){return Number(value)});
 
 switch (operacion) {
     case "sumar":
-        console.log(numero1 + " + " + numero2 + " = " + calculadora.sumar(numero1, numero2));
+        let resultado=calculadora.sumar(...operandosNum);
+        console.log(resultado);            
         break;
+
     case "restar":
-        console.log(numero1 + " - " + numero2 + " = " + calculadora.restar(numero1, numero2));
+        console.log(calculadora.restar(...operandosNum));
         break;
     case "multiplicar":
-        console.log(numero1 + " * " + numero2 + " = " + calculadora.multiplicar(numero1, numero2));
+        console.log(calculadora.multiplicar(...operandosNum));
         break;
     case "dividir":
-        console.log(numero1 + " / " + numero2 + " = " + calculadora.dividir(numero1, numero2));
+        console.log(calculadora.dividir(...operandosNum));
         break;
     case "historial":
-        console.log(calculadora.historial);
+        calculadora.historial();
         break;
+        case "borrar":
+            calculadora.borrar();
+            break;
      default:
         console.log("no se entiende la operación");
         break;
--- calculadora.js
+++ calculadora.js
@@ -1,34 +1,57 @@
 
-function sumar (numero1,numero2){
-    agregarAHistorial()
-    console.log(numero1 + " + " + numero2 + " = " + calculadora.sumar(numero1, numero2));;
+let fs = require("fs");
+let operacionActual;
+function sumar(...operandos) {
+    let resultado= operandos.reduce((res, elem) => { return res + elem; });
+    operacionActual = operandos.reduce((prev, elem) => {return prev+" + "+elem;})+" = "+resultado;
+    agregarAHistorial(operacionActual)
+    return operacionActual;
 }
-function restar (numero1,numero2){
-    return Number(numero1)-Number(numero2);
+function restar(...operandos) {
+    let resultado= operandos.reduce((res, elem) => { return res - elem; });
+    operacionActual = operandos.reduce((prev, elem) => {return prev+" - "+elem;})+" = "+resultado;
+    agregarAHistorial(operacionActual)
+    return operacionActual;
 }
-function multiplicar (numero1,numero2){
-    return Number(numero1)*Number(numero2);
+function multiplicar(...operandos) {
+    let resultado= operandos.reduce((res, elem) => { return res * elem; });
+    operacionActual = operandos.reduce((prev, elem) => {return prev+" * "+elem;})+" = "+resultado;
+    agregarAHistorial(operacionActual)
+    return operacionActual;
 }
-function dividir (numero1,numero2){
-    return Number(numero1)/Number(numero2);
+function dividir(...operandos) {
+    let resultado= operandos.reduce((res, elem) => { return res / elem; });
+    operacionActual = operandos.reduce((prev, elem) => {return prev+" / "+elem;})+" = "+resultado;
+    agregarAHistorial(operacionActual)
+    return operacionActual;
 }
-function leerHistorial (){
-    historialJSON=JSON.parse(fs.readFileSync('./historial.json','uts-8'))
-    console.log (historialJSON);
+function leerLog() {
+    let historialObj = JSON.parse(fs.readFileSync('./historial.json', 'utf-8'))
+    return historialObj;
+}
+function escribirLog(log) {
+    fs.writeFileSync('./historial.json', JSON.stringify(log, null, 4));
 
 }
-function escribirHistorial (){
-    return Number(numero1)/Number(numero2);
-}
-function mostrarHistorial (){
-    return Number(numero1)/Number(numero2);
-}
 
-function agregarAHistorial (){
-    return Number(numero1)/Number(numero2);
+function mostrarHistorial() {
+    let historial = leerLog();
+    console.log(historial);
+    historial.forEach((element, index) => {
+        console.log(index + ": " + element);
+    });
 }
 
+function agregarAHistorial() {
+    let log = leerLog();
+    log.push(operacionActual);
+    // console.log(log);
+    escribirLog(log);
+}
 
+function borrarHistorial(log) {
+    fs.writeFileSync('./historial.json', "[]");
+}
 
 
 module.exports.sumar = sumar;
@@ -36,3 +59,4 @@ module.exports.restar = restar;
 module.exports.multiplicar = multiplicar;
 module.exports.dividir = dividir;
 module.exports.historial = mostrarHistorial;
+module.exports.borrar = borrarHistorial;
